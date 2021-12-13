8086ae7dde035551abf8ec30cf3cd88beb8e53d3
Justin Smith <thejasumasa@gmail.com>
2021-12-13 17:57:10 +0900

    add backend functionality. Cant get httpapi to work

--- src/index.js
+++ src/index.js
@@ -4,34 +4,30 @@ import ReactDOM from 'react-dom';
 import i18n from "i18next";
 import { useTranslation, initReactI18next } from "react-i18next";
 import LanguageDetector from 'i18next-browser-languagedetector';
+import HttpApi from 'i18next-http-backend';
 
 i18n
   .use(initReactI18next)
   .use(LanguageDetector)
+  .use(HttpApi)
   .init({
-    resources: {
-      en: {
-        translation: {
-          "Welcome to React": "Welcome to React and react-i18next"
-        }
-      },
-      jp: {
-        translation: {
-          "Welcome to React": "Reactとreact-i18nextへようこそ！"
-        }
-      }
-    },
     fallbackLng: "en",
     detection: {
       order: ['htmlTag', 'cookie', 'localStorage', 'path', 'subdomain'],
       caches: ['cookie']
-    }
+    },
+    backend: {
+      loadPath:'/assets/locales/{{lng}}/translation.json',
+      allowMultiLoading: false,
+      crossDomain : false
+    },
+    react: { useSuspense: false },
   });
 
 function App() {
   const { t } = useTranslation();
 
-  return <h2>{t('Welcome to React')}</h2>;
+  return <h2>{t('welcome-to-react')}</h2>;
 }
 
 ReactDOM.render(
